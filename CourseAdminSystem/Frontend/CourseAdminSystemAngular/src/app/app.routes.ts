0e73b4a21818d3a1504145f393610fafcaca0254
klaudiabpl-star <klaudiab.pl@gmail.com>
2026-04-21 13:35:11 +0200

    welcom page and end page

--- CourseAdminSystem/Frontend/CourseAdminSystemAngular/src/app/app.routes.ts
+++ CourseAdminSystem/Frontend/CourseAdminSystemAngular/src/app/app.routes.ts
@@ -5,8 +5,13 @@ import { ToiletList } from './toilet-list/toilet-list';
 import { ShitSearch } from './shit-search/shit-search';
 import { ShitInfo } from './shit-info/shit-info';
 import { ShitMap } from './shit-map/shit-map';
+import { Welcome } from './welcome/welcome';
+import { ThankYou } from './thank-you/thank-you';
 
 export const routes: Routes = [
+  { path: '', component: Welcome },
+  { path: 'thank-you', component: ThankYou },
+
   { path: 'shitters', component: ShitterList },
   { path: 'ashits', component: AshitList },
   { path: 'toilet', component: ToiletList },
